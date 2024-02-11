0abbce8ff7e677db026840898443ab085a748091
TETIANAS09 <150555108+TETIANAS09@users.noreply.github.com>
2024-02-11 08:38:01 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,36 @@
+//Створити телефонну книгу
+//створити початковий клас Abonent, де зберігатимуться ім*я і номер
+//створити set який прийматиме телефон і номер
+//створити get який виводитиме данні про абонента
+//створити три різних юзери
+//вивести данні
+
+//result//
+
+/*
+class Abonent {
+  constructor(name, phone) {
+    this.name = name;
+    this.phone = phone;
+  }
+  setName(name) {
+    this.name = name;
+  }
+
+  setPhone(phone) {
+    this.phone = phone;
+  }
+
+  getData() {
+    return `Name: ${this.name}, Phone: ${this.phone}`;
+  }
+}
+const user1 = new Abonent("Tanya", "1234567890");
+const user2 = new Abonent("Ann", "0987654321");
+const user3 = new Abonent("Lili", "1112223333");
+
+console.log(user1.getData());
+console.log(user2.getData());
+console.log(user3.getData());
+
+*/
